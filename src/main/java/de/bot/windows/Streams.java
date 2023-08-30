f8aa14e8f48d09ef24a5dc169a776a01f2dde977
GibMirRechte <mmw3001@gmail.com>
2023-08-30 15:21:23 +0200

    Streams-Redesign

--- src/main/java/de/bot/windows/Streams.java
+++ src/main/java/de/bot/windows/Streams.java
@@ -3,6 +3,7 @@ package de.bot.windows;
 import de.bot.handler.WindowHandler;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -13,6 +14,24 @@ public class Streams extends JPanel {
     public Streams() {
         setPreferredSize(new Dimension(1025, 720));
         setLayout(null);
+
+        JLabel title = new JLabel("Empfohlene Streams");
+
+        title.setVerticalAlignment(SwingConstants.CENTER);
+        title.setForeground(Color.WHITE);
+        title.setHorizontalAlignment(SwingConstants.CENTER);
+
+        add(title);
+
+        title.setBounds(338, 50, 350, 30);
+
+        title.setFont(new Font("Trebuchet MS", Font.PLAIN, 25));
+
+        Border border = BorderFactory.createCompoundBorder(
+                BorderFactory.createMatteBorder(0, 0, 1, 0, Color.GRAY),
+                BorderFactory.createEmptyBorder(5, 10, 5, 10)
+        );
+        title.setBorder(border);
 
         JLabel benDerBaer = new JLabel("<html><center><img src=https://static-cdn.jtvnw.net/jtv_user_pictures/23d9df34-f956-4f8c-bba9-361e19edd4a6-profile_image-70x70.png><br>" +
                 "<br><h1>BenDerBaer</h1>● Gaming<br>● 3D-Design<br><br></center></html>");
@@ -26,9 +45,9 @@ public class Streams extends JPanel {
 
         add(benDerBaer);
 
-        benDerBaer.setBounds(10, 10, 200, 250);
+        benDerBaer.setBounds(410, 100, 200, 250);
         benDerBaer.addMouseListener(addMouseAdapter(benDerBaer, "BenDerBaer"));
-
+/*
         JLabel hakuInu = new JLabel("<html><center><img src=https://static-cdn.jtvnw.net/jtv_user_pictures/23d9df34-f956-4f8c-bba9-361e19edd4a6-profile_image-70x70.png><br>" +
                 "<br><h1>HAKUINU</h1>● 3D-Design<br><br></center></html>");
 
@@ -42,7 +61,7 @@ public class Streams extends JPanel {
         add(hakuInu);
 
         hakuInu.setBounds(220, 10, 200, 250);
-        hakuInu.addMouseListener(addMouseAdapter(hakuInu, "hakuInu"));
+        hakuInu.addMouseListener(addMouseAdapter(hakuInu, "hakuInu"));*/
     }
 
     public static void main(String[] args) {
